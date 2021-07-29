6d567ccb6a8ee9ee24e8e3dfacde92892c75ec6b
rl627 <rl627@drexel.edu>
2021-07-29 11:26:51 -0400

    Gallery: responsive grid for items < 3 #7

--- docs/js/tools.js
+++ docs/js/tools.js
@@ -1,7 +1,7 @@
 //selecting all required elements
 const filterItems = document.querySelectorAll('.techItem');
 const galleryItems = document.querySelectorAll('.toolGallery .toolGalleryItem');
-
+const galleryElement = document.querySelector('.toolGallery');
 // Function to handle click
 const handFilterClick = function (event, array) {
   // Clear Active
@@ -11,19 +11,30 @@ const handFilterClick = function (event, array) {
   button.classList.add('active');
   // Filter Gallery Items
   let filterName = button.getAttribute('data-name');
+  let itemCounter = 0;
   galleryItems.forEach((item) => {
     let itemCat = item.getAttribute('data-name');
     console.log("LOOP ITERATION");
     console.log(item);
     console.log(itemCat);
-    if (itemCat.includes(filterName) || filterName == 'all') {
+    if (filterName == 'all') {
+      itemCounter = galleryItems.length;
+      item.style.display = 'grid'; //first remove the hide class from the image
+    } else if (itemCat.includes(filterName)) {
+      itemCounter = itemCounter + 1;
       item.style.display = 'grid'; //first remove the hide class from the image
     } else {
       item.style.display = 'none'; //first remove the hide class from the image
     }
-    console.log(item.style.display);
-
   });
+  // grid template column update based on number of items
+  if (itemCounter < 3) {
+    galleryElement.classList.remove('toolGallery');
+    galleryElement.classList.add('toolGallerySmall');
+  } else {
+    galleryElement.classList.remove('toolGallerySmall');
+    galleryElement.classList.add('toolGallery');
+  }
 };
 
 // Add listener

a47d9962a0237bef7defe0daaf48bd4fbc86f3d2
rl627 <rl627@drexel.edu>
2021-08-19 16:16:12 -0400

    mobile friendly navbar implemented #17

--- docs/js/navbar.js
+++ docs/js/navbar.js
@@ -15,60 +15,32 @@
 
 var dropdown = document.querySelector('.nav .dropdown');
 var button = document.querySelector('.nav .menu');
-
-/**
- *Is needed to close the dropdown when user clicks outside
- */
 var nav = document.querySelector('.nav');
 
-/**
- * Alternative 1: works with:
- * <i class="material-icons menu" onclick="menu()">menu</i>
- */
 function menu() {
   if (dropdown.style.display === 'grid') {
     dropdown.style.display = 'none';
-    button.innerHTML = '☰';
   } else {
     dropdown.style.display = 'grid';
-    button.innerHTML = '×';
   }
 }
 
-/**
- * Alternative 2: works with: 
- * <i class="material-icons menu">menu</i>
-button.addEventListener('click', function() {
-    if (dropdown.style.display === "grid") {
-    dropdown.style.display = "none";
-    button.innerHTML = "menu";
-  } else {
-    dropdown.style.display = "grid";
-    button.innerHTML = "close";
-  }
-});
- */
-
-/**
- * Close the dropdown (if opened) on window resize
- */
+/** Close the dropdown (if opened) on window resize*/
 window.addEventListener('resize', function () {
   if (window.innerWidth > 768) {
     dropdown.style.display = 'none';
-    button.innerHTML = '☰';
+    button.style.display = 'none';
+  } else {
+    button.style.display = 'grid';
   }
 });
 
-/**
- * If the dropdown is open and a click outside is detected,
- * the dropdown will be closed
- */
+/** If the dropdown is open and a click outside is detected, the dropdown will be closed */
 document.addEventListener('click', function (event) {
   var isClickInside = nav.contains(event.target);
   if (!isClickInside) {
     if (dropdown.style.display === 'grid') {
       dropdown.style.display = 'none';
-      button.innerHTML = '☰';
     }
   }
 });
